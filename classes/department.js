b781dfec134c22c3db3d3c79df574e8a95cca3b0
Justin Douville <jdouville8@gmail.com>
2021-02-24 02:48:57 -0500

    Created department add function; issue with adding to database from there

--- classes/department.js
+++ classes/department.js
@@ -1,1 +1,46 @@
 // require and export whatever classes needed. This does not need to extend from anything
+
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const cTable = require("console.table");
+
+departmentAdd = function () {
+  inquirer
+    .prompt([
+      {
+        name: "id",
+        type: "input",
+        message: "Please provide a unique id",
+      },
+      {
+        name: "name",
+        type: "input",
+        message: "Please provide a department name",
+      },
+    ])
+    .then(function (answer) {
+      // create a new
+      const { id, name } = answer;
+      const newDep = new Department(id, name);
+      newDep.addToDB();
+    });
+};
+
+class Department {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  log() {
+      console.log(this.id);
+      console.log(this.name);
+    }
+
+    //   Create a method that adds new entry to database
+    addToDB() {
+
+    }
+}
+
+module.exports = Department;
--- index.js
+++ index.js
@@ -1,8 +1,81 @@
 const inquirer = require('inquirer')
 const mysql = require('mysql')
 const cTable = require('console.table');
+const Department = require("./classes/department")
 
 
+// create the connection information for the sql database
+// var connection = mysql.createConnection({
+//     host: "localhost",
+
+//     port: 3306,
+
+//     user: "root",
+  
+//     // *****ADD PASSWORD******
+//     password: "",
+//     database: "business_DB"
+//   });
+  
+//   // connect to the mysql server and sql database
+//   connection.connect(function(err) {
+//     if (err) throw err;
+    
+//     initPrompt();
+//   });
 // Create inquirer prompt 
-// Ask user to pick an operation ["Department", "Role" or "Employee")
+// Ask user to pick an operation ["Department", "Role" or "Employee"]
+initPrompt();
+
+function initPrompt() {
+    inquirer
+      .prompt({
+        name: "operation",
+        type: "list",
+        message: "Please pick an operation:",
+        choices: ["Department", "Role", "Employee", "exit"]
+      })
+      .then(function(answer) {
+        // based on their answer, either call the bid or the post functions
+        const { operation } = answer
+        if (operation === "Department") {
+          departmentPrompt();
+        }
+        else if(operation === "Role") {
+          rolePrompt();
+        } 
+        else if(operation === "Employee") {
+          employeePrompt();
+        } 
+        else{
+          connection.end();
+        }
+      });
+  }
     // DEPARTMENT ["View a department", "Add a department"]
+        // in .then, make a new Department object passing in prompt responses (once deconstructed) in as arguments to be used as key value pairs and passed from there into class object methods
+function departmentPrompt() {
+    inquirer
+    .prompt({
+        name: "operation",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["View a department", "Add a department", "exit"]
+         })
+        .then(function(answer) {
+                // based on their answer, either call the bid or the post functions
+            const { operation } = answer
+            if (operation === "View a department") {
+                Department.departmentAdd();
+            }
+            else if(operation === "Add a department") {
+                 departmentAdd();
+            } 
+            else{
+                connection.end();
+            }
+        });
+    }
+
+    // ROLE ["View a role", "Add a role"]
+    // EMPLOYEE ["View an employee", "Add a employee", "Update an employee role"]
